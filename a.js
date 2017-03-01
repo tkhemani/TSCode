3122c2e0a44a385300ad79d468556f0b8209bc72
tarun khemani <tkhemani@gmail.com>
2017-03-02 02:05:14 +0530

    initial

---
+++ a.js
@@ -0,0 +1,11 @@
+var Startup = (function () {
+    function Startup() {
+    }
+    Startup.main = function () {
+        console.log('Hello');
+        return 0;
+    };
+    return Startup;
+}());
+Startup.main();
+//# sourceMappingURL=a.js.map
---
+++ a.ts
@@ -0,0 +1,10 @@
+class Startup {
+    public static main(): number {
+        let a = 10;;
+        if(a==10){}
+        console.log("Hello");
+        return 0;
+    }
+}
+
+Startup.main();
