ce033146fd651a3e77f9282966037d0ebd8e431a
tarun khemani <tkhemani@gmail.com>
2017-03-02 03:46:39 +0530

    POCs

---
+++ circle.js
@@ -0,0 +1,12 @@
+/**
+ * Circle extends IShape
+ */
+var Circle = (function () {
+    function Circle() {
+    }
+    Circle.prototype.getArea = function () {
+        throw new Error("Method not implemented.");
+    };
+    return Circle;
+}());
+//# sourceMappingURL=circle.js.map
---
+++ circle.ts
@@ -0,0 +1,9 @@
+/**
+ * Circle extends IShape
+ */
+class Circle implements IShape {
+    getArea(): number {
+        throw new Error("Method not implemented.");
+    }
+
+}
