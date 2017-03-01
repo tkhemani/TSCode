4a869f56bd1ea9c94e555d75e9edc0009a048c81
tarun khemani <tkhemani@gmail.com>
2017-03-02 03:16:21 +0530

    removing tslint rules coz they were overriding all other rules...also updated to vs code 1.10

--- a.ts
+++ a.ts
@@ -1,10 +1,35 @@
 class Startup {
     public static main(): number {
-        let a = 10;;
-        if(a==10){}
-        console.log("Hello");
+        // let a = 10;;
+        // if(a==10){}
+        console.log("Hi");
         return 0;
     }
 }
 
 Startup.main();
+
+// import fs = require("fs");
+
+// function loadJSONSync(filename: string) {
+//     return JSON.parse(fs.readFileSync(filename));
+// }
+
+// // good json file
+// console.log(loadJSONSync("good.json"));
+
+// // non-existent file, so fs.readFileSync fails
+// try {
+//     console.log(loadJSONSync("absent.json"));
+// }
+// catch (err) {
+//     console.log("absent.json error", err.message);
+// }
+
+// // invalid json file i.e. the file exists but contains invalid JSON so JSON.parse fails
+// try {
+//     console.log(loadJSONSync("invalid.json"));
+// }
+// catch (err) {
+//     console.log("invalid.json error", err.message);
+// }
